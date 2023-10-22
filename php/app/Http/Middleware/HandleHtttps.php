28b309f873a2f55662c266c047d32e8cbde5e837
Irwan Kurniawan <drive.irwanka@gmail.com>
2023-10-22 14:48:38 +0700

    458589

---
+++ php/app/Http/Middleware/HandleHtttps.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+use DB;
+
+//use Illuminate\Http\Request;
+class HandleHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+         
+        if ($request->root()=='http://sicerdas.web.id'){
+            URL::forceSchema("https");
+        }
+        return $next($request);
+    }
+ 
+
+}
