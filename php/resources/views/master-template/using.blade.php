0909b2fd86df56ea6aeda3cae42f534526bc127a
Irwan Kurniawan <drive.irwanka@gmail.com>
2023-11-11 02:33:56 +0700

    Update using.blade.php

--- php/resources/views/master-template/using.blade.php
+++ php/resources/views/master-template/using.blade.php
@@ -1,4 +1,4 @@
-<?php loadHelper('function');?>
+ 
 <div class="row" style="font-size: 0.9em !important;">
     <div class="col-lg-12">
         <table id="datatable-using" class="table table-striped table-hover table-sm" style="width:100%">
@@ -18,7 +18,7 @@
                 <tr>
                     <td><a target="_blank" href="{{url('/manajemen-sesi/detil/'.Crypt::encrypt($r->id_quiz))}}">{{$r->token}}</a></td>
                     <td>{{$r->nama_sesi}}</td>
-                    <td>{{tgl_indo_singkat($r->tanggal)}}</td>
+                    <td>{{$r->tanggal}}</td>
                     <td>{{$r->nama_lokasi}}</td>
                     <td>{{$r->nama_pengguna}}</td>
                     <td>{{$r->kota}}</td>
