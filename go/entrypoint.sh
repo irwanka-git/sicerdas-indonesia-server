ecde31d1de8fe995e1ea90d8174ad7fd265c1b63
Irwan Kurniawan <drive.irwanka@gmail.com>
2023-11-23 00:52:38 +0700

    shhsdfh

--- go/entrypoint.sh
+++ go/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash 
+go build -o /app/utils/publish/main /app/utils/publish/main.go
+echo "Berhasil Build Publish  main.go for cronjob"
+
 go build -o /app/utils/skoring/main /app/utils/skoring/main.go
 echo "Berhasil Build Skoring main.go for cronjob"
 cd /app
