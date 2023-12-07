354a28fc86ead7e147609807882edfc102d3c183
Irwan Kurniawan <drive.irwanka@gmail.com>
2023-12-07 09:49:28 +0700

    wyerui

--- php/resources/views/landing/index.blade.php
+++ php/resources/views/landing/index.blade.php
@@ -124,17 +124,20 @@
                     <?php
                     $tarif_paket = DB::table('tarif_paket')->orderby('id_tarif','asc')->get();
                     ?>
-                   <div class="flex sm:flex-column flex-row space-x-4">
-                    <ul role="list" class="pt-4 space-y-5">
-                        @foreach($tarif_paket as $paket)
-                        <li class="flex space-x-3">
-                            <!-- Icon -->
-                            <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            <span class="text-base  leading-tight text-gray-900 dark:text-white"><b>{{$paket->kode}} - {{$paket->nama_tarif}}</b> <br> @ Rp. {{number_format($paket->tarif,0,",",".")}} /Peserta</span>
-                        </li>
-                        @endforeach
-                    </ul>
-                   </div>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 m-2">
+                
+                @foreach($tarif_paket as $paket)
+                <div class="p-6 bg-white border border-gray-200">
+                    <div class="flex space-x-3">
+                        <!-- Icon -->
+                        <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <span class="text-base  leading-tight text-gray-900 dark:text-white"><b>{{$paket->kode}} - {{$paket->nama_tarif}}</b> <br> @ Rp. {{number_format($paket->tarif,0,",",".")}} /Peserta</span>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+                    
+                   
  
                 </div>
                 </div>
